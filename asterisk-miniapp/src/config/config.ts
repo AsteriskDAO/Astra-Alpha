1015337e870838bc8d3183e7d421e4d3506122d4
Nicholas A Hrboka <nicholashrboka@gmail.com>
2025-05-31 18:39:41 +0200

    updated url

--- asterisk-miniapp/src/config/config.ts
+++ asterisk-miniapp/src/config/config.ts
@@ -3,7 +3,8 @@ const config = {
     server: {
         // TODO: update to production config when needed 
         // self and tg mini apps require https, so we need to use a production server or set up a tunnel for future development
-        url: 'https://asterisk-health-profile.onrender.com'
+        // url: 'https://asterisk-health-profile.onrender.com'
+        url: 'https://asterisk-health-profile.fly.dev/'
     }
 }
 
